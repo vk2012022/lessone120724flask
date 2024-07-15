ac180d537ea172d7af27adce8cecad96f83d461c
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-07-15 17:34:54 +0300

    add new code 150724

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def films():
-    return render_template("index.html") # Внутри () пишем название html-файла в кавычках
+    return render_template("index.html", caption="Фильмы") # Внутри () пишем название html-файла в кавычках
+
+
+@app.route("/shablon")
+def films2():
+    return render_template("index.html", caption="Герои фильмов") # Внутри () пишем название html-файла в кавычках
 
 @app.route("/person/")
 def person():
