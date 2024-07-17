fd7c48294149a278f47054b642e122dbd287c191
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-07-17 14:18:03 +0300

    add new code 170724

--- app1.py
+++ app1.py
@@ -4,11 +4,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    images = [
+        "../static/image/img_6.png",
+        "../static/image/img_7.png",
+        "../static/image/img_8.png"
+    ]
+    accordion_items = [
+        {
+            "title": "Сохранение разнообразия",
+            "content": "Защита и восстановление естественных экосистем и видов, которые в них обитают, с целью поддержания их разнообразия и функциональности."
+        },
+        {
+            "title": "Снижение загрязнения окружающей среды",
+            "content": "Борьба с выбросами вредных веществ в воздух, воду и почву, а также управление отходами для уменьшения их воздействия на природу."
+        },
+        {
+            "title": "Устойчивое использование природных ресурсов",
+            "content": "Рациональное и ответственное использование природных ресурсов, таких как вода, древесина и полезные ископаемые, для обеспечения их доступности для будущих поколений и минимизации разрушения окружающей среды."
+        }
+    ]
+    return render_template('home.html', images=images, accordion_items=accordion_items)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    cards = [
+        {
+            "title": "Заголовок карточки 1",
+            "image": "../static/image/img_2.png",
+            "content": "Пример текста для карточки, который демонстрирует содержание."
+        },
+        {
+            "title": "Заголовок карточки 2",
+            "image": "../static/image/img_3.png",
+            "content": "Пример текста для карточки, который демонстрирует содержание."
+        },
+        {
+            "title": "Заголовок карточки 3",
+            "image": "../static/image/img_5.png",
+            "content": "Пример текста для карточки, который демонстрирует содержание."
+        }
+    ]
+    return render_template('about.html', cards=cards)
 
 if __name__ == '__main__':
     app.run(debug=True)
